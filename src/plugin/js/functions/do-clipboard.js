6fb3d2a677563bc7a2f1d033dd8c24c470e35a28
hayesall <hayesall@iu.edu>
2024-04-18 12:25:34 -0400

    🐛 Fix indentation on copy using hljs table
    The basic idea is that since hljs produces a table where some cells
    contain an `hljs-ln-numbers` class, we can fix the indentation by
    iterating over rows/cells in the table and only keeping `innerText` for
    the non-number rows.

--- src/plugin/js/functions/do-clipboard.js
+++ src/plugin/js/functions/do-clipboard.js
@@ -1,11 +1,29 @@
 export const doClipboard = (options) => {
 
-	let clipboard = options.plaintextonly == true ? 
+	let clipboard = options.plaintextonly == true ?
 	new ClipboardJS(".codeblock > button", {
-		text: function(trigger) { 
+		text: function(trigger) {
 			let code = trigger.nextElementSibling.querySelectorAll('code')[0];
-			return code.innerText.replace(/^\s+|\s+$/g, "")
-		}}) : 
+
+			var table = code.querySelector('table');
+			if (table == null) {
+				return code.innerText.replace(/^\s+|\s+$/g, "")
+			}
+
+			var result = "";
+
+			for (let row of table.rows) {
+				for (let cell of row.cells) {
+					if (cell.className.match("hljs-ln-numbers")) {
+						continue
+					} else {
+						result += (cell.innerText + "\n");
+					}
+				}
+			}
+
+			return result
+		}}) :
 	new ClipboardJS(".codeblock > button", {
 		target({nextElementSibling}) {
 			return nextElementSibling.nextElementSibling.querySelectorAll('code')[0]
